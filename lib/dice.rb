02cd66044250d39327bab27370666df213a54a91
jshields123 <jackshields@live.co.uk>
2021-01-08 12:16:05 +0000

    added rand, multiple dice, array for scores and correct score method. (needs Nil spec)

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,5 +1,16 @@
 class Dice
-  def roll
-     rand(1..6)
+  def roll(num_of_dice = 1)
+    @scores = []
+      num_of_dice.times { @scores << rand(1..5) + 1 }
+      if @scores.size <= 1
+        return @scores.join.to_i
+      else
+      return @scores
   end
+end
+  def total
+    if @scores != nil
+      return @scores.sum
+  end
+end
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -9,4 +9,18 @@ end
   dice = Dice.new
   expect(dice.roll).to be < 7
 end
+  it { is_expected.to respond_to(:roll).with(1).argument }
+
+  it 'gives the correct num of scores' do
+    expect(subject.roll(6).size).to eq 6
+  end
+ it 'gives all the scores between 1 and 6' do
+   expect(subject.roll(9)).to all( be_between(1,6) )
+ end
+  it 'gives total score of dice' do
+    expect(subject.total).to respond_to :total
+  end
+  # it 'if no score return "no score"' do
+  #   expect(subject.total).to raise_error("no score")
+  # end
 end
